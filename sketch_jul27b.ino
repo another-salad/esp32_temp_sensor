195b68173f71f755599abf47643b524567d2920a
jumbobumbo <dwheels91@gmail.com>
2021-07-27 21:45:21 +0100

    initial webserver

---
+++ sketch_jul27b.ino
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <WebServer.h>
+
+/* Put your SSID & Password */
+const char* ssid = "plop";
+const char* password = "Farting_4_Fun!#";
+
+WebServer server(80);
+
+void ConnectToWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to "); Serial.println(ssid);
+
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+
+    if ((++i % 16) == 0)
+    {
+      Serial.println(F(" still trying to connect"));
+    }
+  }
+
+  Serial.print(F("Connected. My IP address is: "));
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  ConnectToWiFi();
+  server.on("/", returnTemp);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void returnTemp() {
+  server.send(200, "text/html", "<html><h1>hello</h1><html>");
+}
