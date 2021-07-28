789a6ee7d03d32bd78f93e327c05f6f59cbd0db7
jumbobumbo <dwheels91@gmail.com>
2021-07-28 14:35:29 +0100

    style consistency

--- sketch_jul27b.ino
+++ sketch_jul27b.ino
@@ -19,13 +19,10 @@ void ConnectToWiFi() {
   Serial.print("Connecting to "); Serial.println(SSID);
 
   uint8_t i = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED){
     Serial.print('.');
     delay(500);
-
-    if ((++i % 16) == 0)
-    {
+    if ((++i % 16) == 0){
       Serial.println(F(" still trying to connect"));
     }
   }
